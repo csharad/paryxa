11d2a74f63d4a1a3ca5a82c7694eba316526d659
Sharad Chand <sharad.d.chand@gmail.com>
2018-10-25 10:23:51 +0530

    Remove useless wrapping and ?

--- src/gql_schema.rs
+++ src/gql_schema.rs
@@ -12,41 +12,39 @@ pub struct Query;
 
 graphql_object!(Query: Context | &self | {
     field users(&executor, query: Option<String>) -> SResult<Vec<User>> {
-        Ok(User::find_all(query, &executor.context().conn)?)
+        User::find_all(query, &executor.context().conn)
     }
 
     field user(&executor, id: Uuid) -> SResult<User> {
-        Ok(User::find_by_uuid(id, &executor.context().conn)?)
+        User::find_by_uuid(id, &executor.context().conn)
     }
 
     field test_papers(&executor) -> SResult<Vec<TestPaper>> {
-        Ok(TestPaper::find_all(&executor.context().conn)?)
+        TestPaper::find_all(&executor.context().conn)
     }
 
     field test_paper(&executor, id: Uuid) -> SResult<TestPaper> {
-        Ok(TestPaper::find_by_uuid(id, &executor.context().conn)?)
+        TestPaper::find_by_uuid(id, &executor.context().conn)
     }
-
-    
 });
 
 pub struct Mutation;
 
 graphql_object!(Mutation: Context | &self | {
     field create_user(&executor, user: UserForm) -> SResult<User> {
-        Ok(user.save(&executor.context().conn)?)
+        user.save(&executor.context().conn)
     }
 
     field update_user(&executor, id: Uuid, user: UserInfoUpdate) -> SResult<User> {
-        Ok(user.save(id, &executor.context().conn)?)
+        user.save(id, &executor.context().conn)
     }
 
     field update_user_type(&executor, id: Uuid, user_type: UserTypeUpdate) -> SResult<User> {
-        Ok(user_type.save(id, &executor.context().conn)?)
+        user_type.save(id, &executor.context().conn)
     }
 
     field delete_user(&executor, id: Uuid) -> SResult<User> {
-        Ok(User::delete_by_uuid(id, &executor.context().conn)?)
+        User::delete_by_uuid(id, &executor.context().conn)
     }
 
     field login(&executor, user: LoginUser) -> SResult<User> {
