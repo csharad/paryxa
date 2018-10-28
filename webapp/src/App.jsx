ede6720f09cef1918ac9fe4465ae8d175989e336
Sharad Chand <sharad.d.chand@gmail.com>
2018-10-28 19:10:01 +0530

    Connect apollo client to the react app

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { AppBar, Toolbar, Typography, CssBaseline, Button, withStyles, createMuiTheme, MuiThemeProvider, colors } from '@material-ui/core';
 import { Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import Home from './Home';
 import Login from './Login';
 import SignUp from './SignUp';
@@ -14,6 +16,10 @@ import InfoBeforeTest from './InfoBeforeTest';
 import TestComplete from './TestComplete';
 import Dashboard from './Dashboard';
 import Settings from './Settings';
+
+const apolloClient = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+});
 
 const theme = createMuiTheme({
     palette: {
@@ -39,34 +45,36 @@ class App extends Component {
         const { classes } = this.props;
 
         return (
-            <MuiThemeProvider theme={theme}>
-                <CssBaseline></CssBaseline>
+            <ApolloProvider client={apolloClient}>
+                <MuiThemeProvider theme={theme}>
+                    <CssBaseline></CssBaseline>
 
-                <AppBar position="sticky">
-                    <Toolbar>
-                        <Typography variant="h6" color="inherit" className={classes.grow}>
-                            Paryksa
+                    <AppBar position="sticky">
+                        <Toolbar>
+                            <Typography variant="h6" color="inherit" className={classes.grow}>
+                                Paryksa
                             </Typography>
 
-                        <Button color="inherit" component={Link} to="/login">Login</Button>
+                            <Button color="inherit" component={Link} to="/login">Login</Button>
 
-                        <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
-                    </Toolbar>
-                </AppBar>
+                            <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
+                        </Toolbar>
+                    </AppBar>
 
-                <Route path="/" component={Home}></Route>
-                <Route path="/login" component={Login}></Route>
-                <Route path="/signup" component={SignUp}></Route>
-                <Route path="/verify-your-account" component={VerifyAccount}></Route>
-                <Route path="/getting-started" component={GettingStarted}></Route>
-                <Route path="/profile" component={Profile}></Route>
-                <Route path="/solved-questions" component={SolvedQuestions}></Route>
-                <Route path="/test-paper" exact component={TestPaper}></Route>
-                <Route path="/test-paper/notice" exact component={InfoBeforeTest}></Route>
-                <Route path="/test-paper/completed" exact component={TestComplete}></Route>
-                <Route path="/dashboard" component={Dashboard}></Route>
-                <Route path="/settings" component={Settings}></Route>
-            </MuiThemeProvider>
+                    <Route path="/" component={Home}></Route>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/signup" component={SignUp}></Route>
+                    <Route path="/verify-your-account" component={VerifyAccount}></Route>
+                    <Route path="/getting-started" component={GettingStarted}></Route>
+                    <Route path="/profile" component={Profile}></Route>
+                    <Route path="/solved-questions" component={SolvedQuestions}></Route>
+                    <Route path="/test-paper" exact component={TestPaper}></Route>
+                    <Route path="/test-paper/notice" exact component={InfoBeforeTest}></Route>
+                    <Route path="/test-paper/completed" exact component={TestComplete}></Route>
+                    <Route path="/dashboard" component={Dashboard}></Route>
+                    <Route path="/settings" component={Settings}></Route>
+                </MuiThemeProvider>
+            </ApolloProvider>
         );
     }
 }
