be88161a2472d072d4fde7b07363a93cba2636be
Sharad Chand <sharad.d.chand@gmail.com>
2018-10-30 17:19:18 +0530

    Implemented working sign up page

--- webapp/src/SignUp.jsx
+++ webapp/src/SignUp.jsx
@@ -9,7 +9,11 @@ import {
     Grid
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withRouter } from 'react-router-dom';
 
 const styles = theme => ({
     paper: {
@@ -32,10 +36,11 @@ const styles = theme => ({
 class SignUp extends Component {
     static propTypes = {
         classes: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired,
     };
 
     render() {
-        const { classes } = this.props;
+        const { classes, history } = this.props;
 
         return (
             <Grid container justify="center">
@@ -52,36 +57,116 @@ class SignUp extends Component {
                         </Typography>
                     </Grid>
 
-                    <form noValidate autoComplete="off">
-                        <TextField
-                            label="E-mail"
-                            fullWidth
-                            margin="normal"
-                        ></TextField>
+                    <Mutation
+                        mutation={gql`
+                            mutation NewUserSignUp($user: UserForm!) {
+                                createUser(user: $user) {
+                                    id
+                                    firstName
+                                    lastName
+                                    email
+                                    gender
+                                    type
+                                }
+                            }
+                        `}
+                    >
+                        {(createUser, { client }) => (
+                            <Formik
+                                initialValues={{
+                                    email: '',
+                                    password: '',
+                                }}
+                                validationSchema={yup.object().shape({
+                                    email: yup.string().email().required(),
+                                    password: yup.string().min(8).required(),
+                                })}
+                                onSubmit={async (user, action) => {
+                                    try {
+                                        await createUser({ variables: { user } });
+                                        // Immediately login the user.
+                                        localStorage.setItem(
+                                            'paryxa-token',
+                                            btoa(`${user.email}:${user.password}`)
+                                        );
+                                        // Due to some reason this query does not replace the `Me`
+                                        // object in state cache. For the time being, gonna use the
+                                        // following workaround.
+                                        await client.reFetchObservableQueries();
+                                        // And redirect to its profile.
+                                        history.push('/profile');
+                                        action.setSubmitting(false);
+                                    } catch (e) {
+                                        if (e.graphQLErrors[0].extensions.kind === 'NOT_UNIQUE') {
+                                            action.setErrors({
+                                                email: 'This e-mail already exists. Enter a new one.'
+                                            });
+                                            return;
+                                        }
+                                        action.setSubmitting(false);
+                                        throw e;
+                                    }
+                                }}
+                            >
+                                {({
+                                    values,
+                                    touched,
+                                    errors,
+                                    handleBlur,
+                                    handleChange,
+                                    handleSubmit,
+                                    isSubmitting,
+                                }) => (
+                                        <form
+                                            noValidate
+                                            autoComplete="off"
+                                            onSubmit={handleSubmit}
+                                        >
+                                            <TextField
+                                                name="email"
+                                                label="E-mail"
+                                                fullWidth
+                                                margin="normal"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.email}
+                                                helperText={touched.email && errors.email}
+                                                error={touched.email && !!errors.email}
+                                            ></TextField>
 
-                        <TextField
-                            label="Password"
-                            fullWidth
-                            type="password"
-                            margin="normal"
-                        ></TextField>
+                                            <TextField
+                                                name="password"
+                                                label="Password"
+                                                fullWidth
+                                                type="password"
+                                                margin="normal"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.password}
+                                                helperText={touched.password && errors.password}
+                                                error={touched.password && !!errors.password}
+                                            ></TextField>
 
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            className={classes.button}
-                            fullWidth
-                            component={Link}
-                            to="/getting-started"
-                        >
-                            Next
-                            <Icon>arrow_right</Icon>
-                        </Button>
-                    </form>
+                                            <Button
+                                                type="submit"
+                                                color="primary"
+                                                variant="contained"
+                                                className={classes.button}
+                                                fullWidth
+                                                disabled={isSubmitting}
+                                            >
+                                                Next
+                                                <Icon>arrow_right</Icon>
+                                            </Button>
+                                        </form>
+                                    )}
+                            </Formik>
+                        )}
+                    </Mutation>
                 </Paper>
             </Grid>
         );
     }
 }
 
-export default withStyles(styles)(SignUp);
+export default withRouter(withStyles(styles)(SignUp));
