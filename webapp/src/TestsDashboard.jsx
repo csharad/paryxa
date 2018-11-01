243529d35ee9a84c06922a6cbafdb8543d24c4e0
Sharad Chand <sharad.d.chand@gmail.com>
2018-11-01 19:32:36 +0530

    A partial impl to list the test papers.

--- webapp/src/TestsDashboard.jsx
+++ webapp/src/TestsDashboard.jsx
@@ -10,6 +10,8 @@ import {
     Typography
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 const styles = theme => ({
     container: {
@@ -28,58 +30,48 @@ class TestsDashboard extends Component {
     render() {
         const { classes } = this.props;
 
+        const tableRow = (paper) => (
+            <TableRow key={paper.id}>
+                <TableCell>{paper.name}</TableCell>
+                <TableCell>{paper.type}</TableCell>
+                <TableCell>Actions</TableCell>
+            </TableRow>
+        );
+
+        const table = (
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Type</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <Query
+                        query={gql`
+                            query TestPaperList {
+                                testPapers {
+                                    id
+                                    name
+                                    type
+                                }
+                            }
+                        `}
+                    >{({ data, loading }) => !loading ? data.testPapers.map(tableRow) : null}
+                    </Query>
+                </TableBody>
+            </Table>
+        );
+
         return (
             <div className={classes.container}>
                 <Typography
-                    variant="display1"
+                    variant="h5"
                     className={classes.title}
-                >Tests
-                </Typography>
+                >Tests</Typography>
 
-                <Paper>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Questions</TableCell>
-                                <TableCell>Total Time</TableCell>
-                                <TableCell>Scheduled On</TableCell>
-                                <TableCell>Actions</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Questions</TableCell>
-                                <TableCell>Total Time</TableCell>
-                                <TableCell>Scheduled On</TableCell>
-                                <TableCell>Actions</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Questions</TableCell>
-                                <TableCell>Total Time</TableCell>
-                                <TableCell>Scheduled On</TableCell>
-                                <TableCell>Actions</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Questions</TableCell>
-                                <TableCell>Total Time</TableCell>
-                                <TableCell>Scheduled On</TableCell>
-                                <TableCell>Actions</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Questions</TableCell>
-                                <TableCell>Total Time</TableCell>
-                                <TableCell>Scheduled On</TableCell>
-                                <TableCell>Actions</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </Paper>
+                <Paper>{table}</Paper>
             </div>
         );
     }
